374b4a4745683a91f070ac6ca1b61b1564debcf2
Misir Jafarov <misir.ceferov@gmail.com>
2025-12-28 23:41:41 +0100

    add sfx

--- themes/paper/static/ko.js
+++ themes/paper/static/ko.js
@@ -28,7 +28,40 @@
   }
 
   let counter = 0;
-  document.querySelector('.btn-dark').addEventListener('click', () => {
-    if (++counter == 6) dok();
+  const sfc = {};
+  sfc.ctx = () => {
+    return sfc._ctx || (sfc._ctx = new (window.AudioContext || window.webkitAudioContext)());
+  }
+  sfc.get = async (url) => {
+    let audio = sfc[url];
+    if (audio) return audio;
+
+    const response = await fetch(url);
+    const buf = await response.arrayBuffer();
+    sfc[url] = audio = await sfc.ctx().decodeAudioData(buf);
+
+    return audio;
+  }
+  sfc.play = (buf) => {
+    const src = sfc.ctx().createBufferSource();
+    src.buffer = buf;
+    src.connect(sfc.ctx().destination);
+    src.start(0);
+  }
+  document.querySelector('.btn-dark').addEventListener('click', async () => {
+    if (++counter == 6) {
+      dok();
+    } else {
+      // credits: http://www.wolfensteingoodies.com/archives/olddoom/music.htm
+      // preload first
+      const dspstop = await sfc.get('/dspstop.wav');
+      const dspstart = await sfc.get('/dspstart.wav');
+
+      // fire
+      if (counter > 1) {
+        const s = isDark ? dspstop : dspstart;
+        sfc.play(s);
+      }
+    }
   });
 })();
